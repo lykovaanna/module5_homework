e0274e764b123e9a4955a2ac7e4331bb75bef486
lykovaanna <138715796+lykovaanna@users.noreply.github.com>
2023-08-01 21:34:21 +0300

    Update task8.js

--- task8.js
+++ task8.js
@@ -4,10 +4,15 @@ map.set('Ivan', '26 y.o.');
 map.set(true, 'student');
 map.set(1, '12 month');
 
-for (let name of map.keys()) {
-    console.log(`${name} - name`)
-};
+// for (let name of map.keys()) {
+//     console.log(`${name} - name`)
+// };
 
-for (let value of map.values()) {
-  console.log(`${value} - values`)
-};
+// for (let value of map.values()) {
+//   console.log(`${value} - values`)
+// };
+
+map.forEach((value, key) => {
+            console.log(`${key} - x = ${value} - y`); 
+});
+     
